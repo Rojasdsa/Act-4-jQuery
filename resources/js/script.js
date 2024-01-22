fa5da61a8a4d70eb237517da4da565941080d3fc
Fernando Rojas <rojasdsa@alumnos.ilernasevilla.com>
2024-01-22 23:18:27 +0100

    FIN REAL ACORDEON

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,53 +1,35 @@
 'use strict'
 
 // ACORDEÓN
-
 $(document).ready(function () {
-
     // Ocultar todos los contenidos al cargar la página
-    /* 
-    Estaba añadiendo la clase d-none y como interfiere con las animaciones de jquery,
-    lo he modificado por la funcion .hide()*/
     $('.empresa-acord').hide();
 
     // Manejar clics en títulos
     $('.titulo').click(function () {
-        // Guardar el contenido actual en una variable
-        let contenidoActual = $(this).next();
+        // Ocultar todos los elementos .empresa-acord excepto el que se ha pulsado
+        $('.empresa-acord').not($(this).next()).slideUp();
 
-        /* 
-        Utilizando find(), estamos buscando todos los elementos a 
-        dentro del contenidoActual. Estos son los enlaces dentro del contenido.
-        */
-        let enlacesEnContenido = contenidoActual.find('a');
-        console.log('Enlaces dentro del contenido:', enlacesEnContenido);
-
-        /* 
-        Utilizando children(), estamos obteniendo todos los hijos directos del contenidoActual.
-        */
-        let hijosDirectosDeContenido = contenidoActual.children();
-        console.log('Hijos directos del contenido:', hijosDirectosDeContenido);
-
-        /* 
-        Utilizando closest(), estamos buscando el ancestro más cercano 
-        del contenidoActual que tenga la clase item. Esto es útil para encontrar 
-        el contenedor padre del contenido actual.
-        */
-        let contenedorPadre = contenidoActual.closest('.item');
-        console.log('Contenedor padre:', contenedorPadre);
-
-
-        // Verificar si el contenido actual está visible
-        if (contenidoActual.is(':visible')) {
-            // Si está visible, realizar slideUp()
-            contenidoActual.slideUp();
-        } else {
-            // Si no está visible, ocultar todos los contenidos y realizar slideDown()
-            $('.empresa-acord').not(contenidoActual).slideUp();
-            contenidoActual.slideDown();
-        }
+        // Mostrar o ocultar el contenido del elemento pulsado
+        $(this).next().slideToggle();
     });
 });
 
 
+
 // MOUSEOVER / MOUSEOUT IMG
+/* 
+mouseenter y mouseleave son eventos de mouse en jQuery que se utilizan 
+para manejar las interacciones del mouse con un elemento del DOM. 
+*/
+
+$(document).ready(function () {
+    $('.img-container').on('mouseenter', function () {
+        let text = $(this).data('text');
+        $(this).find('.overlay-text').text(text).fadeIn();
+        $(this).find('.bg-overlay-text').fadeIn().css('opacity', '0.7'); // Cambia 0.5 al valor de opacidad que desees
+    }).on('mouseleave', function () {
+        $(this).find('.overlay-text').fadeOut();
+        $(this).find('.bg-overlay-text').fadeIn().css('opacity', '1'); // Cambia 0.5 al valor de opacidad que desees
+    });
+});
