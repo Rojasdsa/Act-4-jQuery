91ed4468852c7dca86538bcc5fff24c8bab6604a
Fernando Rojas <rojasdsa@alumnos.ilernasevilla.com>
2024-01-22 20:54:36 +0100

    fin acordeon. Falta mouseover/mouseout

--- resources/js/script.js
+++ resources/js/script.js
@@ -2,18 +2,52 @@
 
 // ACORDEÓN
 
-$(document).ready(function() {
-    // Manejar clics en títulos
-    $('.titulo').click(function() {
-        // Ocultar todos los contenidos y mostrar solo el del clic actual
-        $('.contenido').not($(this).next()).slideUp();
-        $(this).next().slideToggle();
+$(document).ready(function () {
 
-        // Verificar cuál contenido está actualmente visible
-        $('.contenido').each(function() {
-            if ($(this).is(':visible')) {
-                console.log('El contenido de este apartado está visible:', $(this).text());
-            }
-        });
+    // Ocultar todos los contenidos al cargar la página
+    /* 
+    Estaba añadiendo la clase d-none y como interfiere con las animaciones de jquery,
+    lo he modificado por la funcion .hide()*/
+    $('.empresa-acord').hide();
+
+    // Manejar clics en títulos
+    $('.titulo').click(function () {
+        // Guardar el contenido actual en una variable
+        let contenidoActual = $(this).next();
+
+        /* 
+        Utilizando find(), estamos buscando todos los elementos a 
+        dentro del contenidoActual. Estos son los enlaces dentro del contenido.
+        */
+        let enlacesEnContenido = contenidoActual.find('a');
+        console.log('Enlaces dentro del contenido:', enlacesEnContenido);
+
+        /* 
+        Utilizando children(), estamos obteniendo todos los hijos directos del contenidoActual.
+        */
+        let hijosDirectosDeContenido = contenidoActual.children();
+        console.log('Hijos directos del contenido:', hijosDirectosDeContenido);
+
+        /* 
+        Utilizando closest(), estamos buscando el ancestro más cercano 
+        del contenidoActual que tenga la clase item. Esto es útil para encontrar 
+        el contenedor padre del contenido actual.
+        */
+        let contenedorPadre = contenidoActual.closest('.item');
+        console.log('Contenedor padre:', contenedorPadre);
+
+
+        // Verificar si el contenido actual está visible
+        if (contenidoActual.is(':visible')) {
+            // Si está visible, realizar slideUp()
+            contenidoActual.slideUp();
+        } else {
+            // Si no está visible, ocultar todos los contenidos y realizar slideDown()
+            $('.empresa-acord').not(contenidoActual).slideUp();
+            contenidoActual.slideDown();
+        }
     });
 });
+
+
+// MOUSEOVER / MOUSEOUT IMG
