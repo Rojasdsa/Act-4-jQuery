c4f5ed017e02d6ab878720f98f984eaa165d080d
Fernando Rojas <rojasdsa@alumnos.ilernasevilla.com>
2024-01-22 19:27:21 +0100

    acordeon jquery

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,1 +1,19 @@
 'use strict'
+
+// ACORDEÓN
+
+$(document).ready(function() {
+    // Manejar clics en títulos
+    $('.titulo').click(function() {
+        // Ocultar todos los contenidos y mostrar solo el del clic actual
+        $('.contenido').not($(this).next()).slideUp();
+        $(this).next().slideToggle();
+
+        // Verificar cuál contenido está actualmente visible
+        $('.contenido').each(function() {
+            if ($(this).is(':visible')) {
+                console.log('El contenido de este apartado está visible:', $(this).text());
+            }
+        });
+    });
+});
