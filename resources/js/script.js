16ea74846951952bfb1fa27e36fe469cb8460626
Fernando Rojas <rojasdsa@alumnos.ilernasevilla.com>
2024-01-23 02:59:02 +0100

    fin de actividad

--- resources/js/script.js
+++ resources/js/script.js
@@ -33,3 +33,30 @@ $(document).ready(function () {
         $(this).find('.bg-overlay-text').fadeIn().css('opacity', '1'); // Cambia 0.5 al valor de opacidad que desees
     });
 });
+
+
+// EXTRA
+
+$(document).ready(function () {
+    // Ejemplo 1: animate()
+    $('#example1-box').click(function () {
+      $(this).animate({ width: '200px', height: '200px' }, 1000);
+    });
+
+    // Ejemplo 2: scroll()
+    $(window).scroll(function () {
+      console.log('Has hecho scroll');
+    });
+
+    // Ejemplo 3: resize()
+    $(window).resize(function () {
+      let width = $(window).width();
+      let height = $(window).height();
+      console.log('Ancho: ' + width + ', Altura: ' + height);
+    });
+
+    // Ejemplo 4: attr()
+    $('#change-attr-btn').click(function () {
+        $('#my-link').attr('href', 'https://www.ilerna.es');
+      });
+  });
